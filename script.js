baada0ce95f6d987e063616f711ec53a8c81229f
Jory <46983170+Cambro320@users.noreply.github.com>
2019-12-10 10:15:11 -0600

    Added initial files

---
+++ script.js
@@ -0,0 +1,78 @@
+// var button = document.getElementsByTagName("button")[0];
+
+// button.addEventListener("mouseenter", function() {
+//   console.log("Click!!!!!");
+// })
+
+// cache get elements/queried selectors into variables
+var button = document.getElementById("enter");
+var input = document.getElementById("userinput");
+
+var ul = document.querySelector("ul"); // to append li to it 
+var list_li = document.querySelectorAll("li");
+var delButton = document.getElementsByClassName("delete");
+
+// refactoring: make code better by using functions to abstract repetitive code blocks
+function inputLength() {
+  return input.value.length; // check if input is blank
+}
+
+function createListElement() {
+  var li = document.createElement("li"); 
+  var addButton = document.createElement("button");
+  addButton.innerHTML = "X";
+
+  li.appendChild(document.createTextNode(input.value.charAt(0).toUpperCase() + input.value.slice(1))); //capitalize and add delete button    
+  li.innerHTML = li.innerHTML + "----";
+  li.appendChild(addButton);
+  li.addEventListener("click", toggleLiClass);
+  ul.appendChild(li); 
+  input.value = "";  
+}
+
+function addListAfterClick() {
+  if (inputLength() > 0 ) {
+    createListElement();
+  }
+}
+
+function addListAfterKeypress(event) {
+  if (inputLength() > 0 && event.keyCode === 13) {  
+      createListElement();
+  }
+}
+
+function toggleLiClass() {
+  this.classList.toggle("done");
+}
+
+function addLiEventListener(){
+  for (var i = 0; i < list_li.length; i++) {
+    list_li[i].addEventListener("click", toggleLiClass);
+  };
+}
+
+addLiEventListener()
+
+button.addEventListener("click", addListAfterClick);
+
+input.addEventListener("keypress", addListAfterKeypress);
+
+// Original blocks of code:
+
+// button.addEventListener("click", function() {
+//   if (input.value.length > 0 ) {  // making sure input isn't blank, will add blank to list without this check
+//     var li = document.createElement("li"); // create new li
+//     li.appendChild(document.createTextNode(input.value)); // add input.value to new li 
+//     ul.appendChild(li);
+//     input.value = ""; // clear input text box
+//   }  
+// });
+
+// input.addEventListener("keypress", function(event) {
+//   var li = document.createElement("li");
+//   li.appendChild(document.createTextNode(input.value)); 
+//   ul.appendChild(li);
+//   input.value = ""; 
+// }
+
