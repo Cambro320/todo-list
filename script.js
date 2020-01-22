5e85cbca4623de46a107e819a1628ba09e33e3e8
Jory <46983170+Cambro320@users.noreply.github.com>
2020-01-22 08:12:01 -0600

    updated styling

--- script.js
+++ script.js
@@ -6,21 +6,20 @@
 
 // cache get elements/queried selectors into variables
 var button = document.getElementById("enter");
-var input = document.getElementById("userinput"); 
+var input = document.getElementById("userinput");
 
-var ul = document.querySelector("ul"); // to append li to it 
+var ul = document.querySelector("ul"); // to append li to it
 var list_li = document.querySelectorAll("li");
 var delButton = document.getElementsByClassName("delete");
 
-
-// Add click listener to current li 
+// Add click listener to current li
 for (var i = 0; i < list_li.length; i++) {
   list_li[i].addEventListener("click", toggleLiClass);
 }
 
-// Add click listener to current delete button 
-for(var i = 0; i < delButton.length; i++){
-	delButton[i].addEventListener("click", delParent);
+// Add click listener to current delete button
+for (var i = 0; i < delButton.length; i++) {
+  delButton[i].addEventListener("click", delParent);
 }
 
 // refactoring: using functions to abstract repetitive code blocks
@@ -35,29 +34,33 @@ function inputLength() {
 }
 
 function createListElement() {
-  var li = document.createElement("li"); 
+  var li = document.createElement("li");
   var addButton = document.createElement("button");
   addButton.classList.add("delete");
   addButton.innerHTML = "X";
   addButton.addEventListener("click", delParent);
 
-  li.appendChild(document.createTextNode(input.value.charAt(0).toUpperCase() + input.value.slice(1))); //capitalize and add delete button    
-  li.innerHTML = li.innerHTML + "----";  
+  li.appendChild(
+    document.createTextNode(
+      input.value.charAt(0).toUpperCase() + input.value.slice(1)
+    )
+  ); //capitalize and add delete button
+  li.innerHTML = li.innerHTML + "------";
   li.appendChild(addButton);
   li.addEventListener("click", toggleLiClass);
-  ul.appendChild(li); 
-  input.value = "";  
+  ul.appendChild(li);
+  input.value = "";
 }
 
 function addListAfterClick() {
-  if (inputLength() > 0 ) {
+  if (inputLength() > 0) {
     createListElement();
   }
 }
 
 function addListAfterKeypress(event) {
-  if (inputLength() > 0 && event.keyCode === 13) {  
-      createListElement();
+  if (inputLength() > 0 && event.keyCode === 13) {
+    createListElement();
   }
 }
 
@@ -73,16 +76,15 @@ input.addEventListener("keypress", addListAfterKeypress);
 // button.addEventListener("click", function() {
 //   if (input.value.length > 0 ) {  // making sure input isn't blank, will add blank to list without this check
 //     var li = document.createElement("li"); // create new li
-//     li.appendChild(document.createTextNode(input.value)); // add input.value to new li 
+//     li.appendChild(document.createTextNode(input.value)); // add input.value to new li
 //     ul.appendChild(li);
 //     input.value = ""; // clear input text box
-//   }  
+//   }
 // });
 
 // input.addEventListener("keypress", function(event) {
 //   var li = document.createElement("li");
-//   li.appendChild(document.createTextNode(input.value)); 
+//   li.appendChild(document.createTextNode(input.value));
 //   ul.appendChild(li);
-//   input.value = ""; 
+//   input.value = "";
 // }
-
